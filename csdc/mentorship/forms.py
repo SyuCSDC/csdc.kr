37fd4eda8c6cc82cc64e04d2f076bcb01d01656b
minseong0213 <minseong0213@naver.com>
2024-02-14 02:08:22 +0900

    fix: modify book request & book_list / post_list & post_create / mentorship widget field

--- csdc/mentorship/forms.py
+++ csdc/mentorship/forms.py
@@ -6,14 +6,17 @@ class CustomModelChoiceField(forms.ModelChoiceField):
     def label_from_instance(self, obj):
         # obj는 UserProfile 객체입니다. 여기서 원하는 문자열 형태로 반환합니다.
         return f"{obj.student_id} - {obj.user.last_name}{obj.user.first_name}"
+    
 
 class MentorshipForm(forms.ModelForm):
     mentee = CustomModelChoiceField(
         queryset=UserProfile.objects.filter(role='Mentee').order_by('student_id'),
         label="멘티 선택",
         help_text="멘토링을 받을 멘티를 선택해주세요.",
+        empty_label="멘티 선택",
         widget=forms.Select(attrs={'name': 'mentee'})
     )
+
     
     class Meta:
         model = Mentorship
@@ -21,13 +24,10 @@ class MentorshipForm(forms.ModelForm):
         widgets = {
             'start_date': forms.DateInput(attrs={'type': 'date'}),
             'end_date': forms.DateInput(attrs={'type': 'date'}),
+            'status': forms.Select(attrs={'placeholder': 'Add notes to question here'}),
         }
 
-    # def __init__(self, *args, **kwargs):
-    #     super(MentorshipForm, self).__init__(*args, **kwargs)
-        
-    #     # 멘티 선택 필드에 학번과 이름을 함께 표시하기 위해 옵션 리스트를 생성합니다.
-    #     mentee_choices = [(profile.student_id, f"{profile.student_id} - {profile.user.last_name}{profile.user.first_name} ") for profile in UserProfile.objects.filter(role='Mentee')]
-        
-    #     # 선택 필드의 choices 속성을 설정합니다.
-    #     self.fields['mentee'].choices = mentee_choices
+    def __init__(self, *args, **kwargs):
+        super(MentorshipForm, self).__init__(*args, **kwargs)
+        self.fields['book'].empty_label = "책을 선택해주세요."
+        self.fields['status'].empty_label = None
