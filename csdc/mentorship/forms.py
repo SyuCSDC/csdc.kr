0748274856eb14dcac8aecfae9647800798025e2
minseong0213 <minseong0213@naver.com>
2024-02-15 19:55:58 +0900

    fix: mentorship_create some modified

--- csdc/mentorship/forms.py
+++ csdc/mentorship/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Mentorship 
+from django.core.exceptions import ValidationError
 from user.models import UserProfile
 
 class CustomModelChoiceField(forms.ModelChoiceField):
@@ -24,6 +25,17 @@ class MentorshipForm(forms.ModelForm):
             'start_date': forms.DateInput(attrs={'type': 'date'}),
             'end_date': forms.DateInput(attrs={'type': 'date'}),
         }
+
+    def clean(self):
+        cleaned_data = super().clean()
+        start_date = cleaned_data.get("start_date")
+        end_date = cleaned_data.get("end_date")
+
+        # 시작 날짜와 종료 날짜 비교
+        if end_date and start_date and end_date < start_date:
+            raise ValidationError("종료 날짜는 시작 날짜보다 뒤에 있어야 합니다.")
+
+        return cleaned_data
 
     def __init__(self, *args, **kwargs):
         super(MentorshipForm, self).__init__(*args, **kwargs)
