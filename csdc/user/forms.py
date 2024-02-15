040087b37438233c52c1c7c5a20b2f2108beabe4
0verfl0w767 <tkddbs7631@naver.com>
2024-02-15 13:57:08 +0900

    feat: auth validation

--- csdc/user/forms.py
+++ csdc/user/forms.py
@@ -10,14 +10,50 @@ class UserLoginForm(AuthenticationForm):
         self.fields['username'].label = '아이디'
 
 class UserRegisterForm(UserCreationForm):
-    grade = forms.CharField(max_length=10, required=False, label="학년")
-    student_id = forms.CharField(max_length=20, required=False, label="학번")
-    bio = forms.CharField(widget=forms.Textarea, required=False, label="한 줄 소개")
-    role = forms.ChoiceField(choices=(('Mentor', 'Mentor'), ('Mentee', 'Mentee')), required=True, label="Role")
+    grade = forms.CharField(max_length=10, required=False, label='학년')
+    student_id = forms.CharField(max_length=20, required=False, label='학번')
+    role = forms.ChoiceField(choices=(('Mentor', 'Mentor'), ('Mentee', 'Mentee')), required=True, label='Role')
+    bio = forms.CharField(widget=forms.Textarea, required=False, label='한 줄 소개')
 
     class Meta:
         model = User
         fields = ['username','last_name','first_name' ,'email', 'password1', 'password2','grade', 'student_id' ,'role', 'bio']
+    
+    def clean_last_name(self):
+        last_name = self.cleaned_data['last_name']
+        if not last_name:
+            raise forms.ValidationError('성을 입력해주세요.')
+        return last_name
+    
+    def clean_first_name(self):
+        first_name = self.cleaned_data['first_name']
+        if not first_name:
+            raise forms.ValidationError('이름을 입력해주세요.')
+        return first_name
+    
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if not email:
+            raise forms.ValidationError('이메일을 입력해주세요.')
+        return email
+    
+    def clean_grade(self):
+        grade = self.cleaned_data['grade']
+        if not grade:
+            raise forms.ValidationError('학년을 입력해주세요.')
+        return grade
+    
+    def clean_student_id(self):
+        student_id = self.cleaned_data['student_id']
+        if not student_id:
+            raise forms.ValidationError('학번을 입력해주세요.')
+        return student_id
+    
+    def clean_bio(self):
+        bio = self.cleaned_data['bio']
+        if not bio:
+            raise forms.ValidationError('한 줄 소개를 입력해주세요.')
+        return bio
 
     def save(self, commit=True):
         user = super().save(commit=False)
@@ -25,7 +61,6 @@ class UserRegisterForm(UserCreationForm):
         user.last_name = self.cleaned_data['last_name']
         if commit:
             user.save()
-            # User 모델 저장 후 UserProfile 모델에 데이터 저장
             UserProfile.objects.create(
                 user=user,
                 grade=self.cleaned_data['grade'],
@@ -37,14 +72,11 @@ class UserRegisterForm(UserCreationForm):
     
     def __init__(self, *args, **kwargs):
         super(UserRegisterForm, self).__init__(*args, **kwargs)
-        self.fields['username'].label = '아이디'
-        self.fields['username'].widget.attrs.update({'class': 'form-control'})
-        self.fields['last_name'].widget.attrs.update({'class': 'form-control'})
-        self.fields['first_name'].widget.attrs.update({'class': 'form-control'})
-        self.fields['email'].widget.attrs.update({'class': 'form-control'})
-        self.fields['password1'].widget.attrs.update({'class': 'form-control'})
-        self.fields['password2'].widget.attrs.update({'class': 'form-control'})
-        self.fields['grade'].widget.attrs.update({'class': 'form-control'})
-        self.fields['student_id'].widget.attrs.update({'class': 'form-control'})
-        self.fields['role'].widget.attrs.update({'class': 'form-select'})
-        self.fields['bio'].widget.attrs.update({'class': 'form-control'})
+        
+        for field in self.fields:
+            if field == 'username':
+                self.fields[field].label = '아이디'
+            if field == 'role':
+                self.fields[field].widget.attrs.update({'class': 'form-select'})
+            self.fields[field].widget.attrs.update({'class': 'form-control'})
+        
