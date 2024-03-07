e71365fa2fb62ff30745e74ad53f14ba578cd850
0verfl0w767 <tkddbs7631@naver.com>
2024-03-08 01:49:10 +0900

    fix: mentorship form validation

--- csdc/mentorship/forms.py
+++ csdc/mentorship/forms.py
@@ -48,15 +48,12 @@ class MentorshipForm(forms.ModelForm):
         cleaned_data = super().clean()
         start_date = cleaned_data.get("start_date")
         end_date = cleaned_data.get("end_date")
+        mentees = cleaned_data.get('mentees')
 
         # 시작 날짜와 종료 날짜 비교
         if end_date and start_date and end_date < start_date:
             raise ValidationError("종료 날짜는 시작 날짜보다 뒤에 있어야 합니다.")
-
-        return cleaned_data
-    
-    def clean(self):
-        mentees = self.cleaned_data.get('mentees', [])
+        
         if not mentees:
             raise ValidationError("멘티를 선택해야 합니다.")
         
@@ -64,7 +61,6 @@ class MentorshipForm(forms.ModelForm):
         for mentee in mentees:
             if not UserProfile.objects.filter(pk=mentee.pk, role='Mentee').exists():
                 raise ValidationError(f"선택된 멘티 중 '{mentee}'은(는) 유효하지 않은 값입니다.")
-        
-        return mentees
-        
 
+        return cleaned_data
+    
