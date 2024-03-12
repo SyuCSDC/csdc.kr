4c3c5b867cfda255a1eb34a911b38b691fd1708f
0verfl0w767 <tkddbs7631@naver.com>
2024-03-13 00:39:17 +0900

    rebase: config/settings.py & user/views.py

--- csdc/user/views.py
+++ csdc/user/views.py
@@ -29,3 +29,35 @@ class UserRegisterView(TemplateView):
         login(request, user)
         
         return redirect('/')
+
+
+class MyPasswordResetView(PasswordResetView):
+    subject_template_name='user/password_reset_subject.txt',  # 수정: 파일 이름을 제목용으로 변경
+    email_template_name='user/password_reset_email.html',
+    success_url = reverse_lazy('user:password_reset_done') 
+
+class MyPasswordResetChangeView(PasswordResetConfirmView):
+    success_url = reverse_lazy('user:password_reset_complete')
+
+
+
+class MyforgotidView(TemplateView):
+    template_name = 'user/forgot_id.html'
+
+    def get(self, request):
+        return render(request, self.template_name)
+
+    def post(self, request):
+        context = {}
+        email = request.POST.get('email')
+        student_id = request.POST.get('student_id')
+
+        try:
+            user_profile = UserProfile.objects.get(user__email=email, student_id=student_id)
+            print(user_profile)
+            user_id = user_profile.user.username # 또는 필요한 다른 식별자
+            context['user_id'] = user_id  
+        except UserProfile.DoesNotExist:
+            context['error'] = "No user found with the provided information."
+
+        return render(request , self.template_name, context)       
