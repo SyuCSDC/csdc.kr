984e2db08b893cc61fa19d5fae57f0e48f742e7e
0verfl0w767 <tkddbs7631@naver.com>
2024-03-14 03:38:20 +0900

    feat: show superuser ( developer ) profile

--- csdc/config/views.py
+++ csdc/config/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 
-from user.models import UserProfile
+from user.models import User, UserProfile
 
 def index(request):
     return render(request, 'index.html')
 
 def about(request):
-    return render(request, 'about.html', { 'users': UserProfile.objects.filter(role='Mentor') })
+    return render(request, 'about.html', { 'admins': User.objects.filter(is_superuser=True), 'users': UserProfile.objects.filter(role='Mentor') })
